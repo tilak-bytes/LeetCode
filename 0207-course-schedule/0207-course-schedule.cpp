b6a117cec20b9c94f9d15fab23e4f546bc06de22
Tilak Verma <engineer.tilak2027@gmail.com>
2026-06-29 15:02:15 +0530

    Time: 7 ms (45.35%), Space: 19.3 MB (54.87%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,36 +1,37 @@
 class Solution {
 public:
+
+    bool hasCycle(int node, vector<int>& state, vector<vector<int>>& adj) {
+        state[node] = 1;
+
+        for(auto neighbor : adj[node]){
+            if(state[neighbor] == 1) return true;
+
+            if(state[neighbor] == 0)
+                if(hasCycle(neighbor, state, adj) == true) return true;
+        }
+        state[node] = 2;
+        return false;
+    }
+
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         int n = prerequisites.size();
+
         vector<vector<int>> adj(numCourses);
 
-        vector<int> indegree(numCourses, 0);
-
-        for(int i = 0 ; i < n ; i++){
+        for(int i = 0 ; i < n ; i++) {
             int u = prerequisites[i][0];
             int v = prerequisites[i][1];
 
-            adj[v].push_back(u);   //  v → u (v must come before u)
-            indegree[u]++;         // u has a dependency
+            adj[v].push_back(u);        // v → u
         }
 
-        queue<int> q;
+        vector<int> state(numCourses, 0);
 
         for(int i = 0 ; i < numCourses ; i++)
-            if(indegree[i] == 0) q.push(i);
+            if(state[i] == 0)
+                if(hasCycle(i, state, adj)) return false;
 
-        int count = 0;
-        while(!q.empty()){
-            int node = q.front();
-            q.pop();
-            count++;
-
-            for(auto neighbor : adj[node]){
-                indegree[neighbor]--;
-                if(indegree[neighbor] == 0) q.push(neighbor);
-            }
-        }
-
-        return (count == numCourses);
+        return true;  
     }
 };
