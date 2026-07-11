e31032f77a51853fc43e34d1d82039f338684abd
Tilak Verma <engineer.tilak2027@gmail.com>
2026-07-11 13:24:50 +0530

    Time: 0 ms (100%), Space: 16.5 MB (16.76%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -31,6 +31,26 @@ private:
         return dp[i][j] = grid[i][j] + min(up, left);
     }
 
+    int solveTab(int m, int n, vector<vector<int>>& grid) {
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(i == 0 && j == 0) {
+                    dp[i][j] = grid[i][j];
+                    continue;
+                }
+
+                int up = (i > 0) ? dp[i - 1][j] : INT_MAX;
+                int left = (j > 0) ? dp[i][j - 1] : INT_MAX;
+
+                dp[i][j] = grid[i][j] + min(up, left);
+            }
+        }
+
+        return dp[m - 1][n - 1];
+    }
+
 public:
     int minPathSum(vector<vector<int>>& grid) {
         int m  = grid.size();
@@ -39,7 +59,9 @@ public:
         // return solveRec(m - 1, n - 1, grid);
         //return solveRec2(m - 1, n - 1, grid);
 
-        vector<vector<int>> dp(m, vector<int>(n, -1));
-        return solveMem(m - 1, n - 1, grid, dp);
+        //vector<vector<int>> dp(m, vector<int>(n, -1));
+        //return solveMem(m - 1, n - 1, grid, dp);
+
+        return solveTab(m, n, grid);
     }
 };
