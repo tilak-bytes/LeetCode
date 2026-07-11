77609a7a35a6f46f1a925532666391033e4e1c48
Tilak Verma <engineer.tilak2027@gmail.com>
2026-07-11 13:21:58 +0530

    Time: 3 ms (30.14%), Space: 16.4 MB (16.76%) - LeetHub

---
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -0,0 +1,45 @@
+class Solution {
+private:
+    int solveRec(int i, int j, vector<vector<int>>& grid) {
+        if(i == 0 && j == 0) return grid[i][j];
+        if(i < 0 || j < 0) return INT_MAX - 200;     //Subtracted 200 to prevent overflow as max value can be 200.
+
+        int up = grid[i][j] + solveRec(i - 1, j, grid);
+        int left = grid[i][j] + solveRec(i, j - 1, grid);
+
+        return min(up, left);
+    }
+
+    //Another way to prevent overflow in recursive solution is:
+    int solveRec2(int i, int j, vector<vector<int>>& grid) {
+        if(i == 0 && j == 0) return grid[i][j];
+        if(i < 0 || j < 0) return INT_MAX;
+
+        int up = (i > 0) ? solveRec2(i - 1, j, grid) : INT_MAX;
+        int left = (j > 0) ? solveRec2(i, j - 1, grid) : INT_MAX;
+
+        return grid[i][j] + min(up, left);
+    }
+
+    int solveMem(int i, int j, vector<vector<int>>& grid, vector<vector<int>>& dp) {
+        if(i == 0 && j == 0) return grid[i][j];
+        if(dp[i][j] != -1) return dp[i][j];
+
+        int up = (i > 0) ? solveMem(i - 1, j, grid, dp) : INT_MAX;
+        int left = (j > 0) ? solveMem(i, j - 1, grid, dp) : INT_MAX;
+
+        return dp[i][j] = grid[i][j] + min(up, left);
+    }
+
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        int m  = grid.size();
+        int n = grid[0].size();
+
+        // return solveRec(m - 1, n - 1, grid);
+        //return solveRec2(m - 1, n - 1, grid);
+
+        vector<vector<int>> dp(m, vector<int>(n, -1));
+        return solveMem(m - 1, n - 1, grid, dp);
+    }
+};
