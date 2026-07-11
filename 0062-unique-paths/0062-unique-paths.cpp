5a62581fee90fb13aae5fc7a6f1903d3902356e4
Tilak Verma <engineer.tilak2027@gmail.com>
2026-07-11 12:23:16 +0530

    Time: 2 ms (20.92%), Space: 8.3 MB (82.41%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -43,6 +43,29 @@ private:
         return dp[m - 1][n - 1];
     }
 
+    int solveSO(int m, int n) {
+        vector<int> prev(n, 0);
+
+        for(int i = 0; i < m; i++) {
+
+            vector<int> curr(n, 0);
+            for(int j = 0 ; j < n ; j++) {
+
+                if(i == 0 && j == 0) {
+                    curr[j] = 1;
+                    continue;
+                }
+
+                int up = prev[j];
+                int left = (j > 0) ? curr[j - 1] : 0;
+
+                curr[j] = up + left;
+            }
+            prev = curr;
+        }
+        return prev[n - 1];
+    }
+
 public:
     int uniquePaths(int m, int n) {
         //return solveRec(m - 1, n - 1);
@@ -50,6 +73,8 @@ public:
         //vector<vector<int>> dp(m, vector<int>(n, -1));
         //return solveMem(m - 1, n - 1, dp);
 
-        return solveTab(m, n);
+        //return solveTab(m, n);
+
+        return solveSO(m, n);
     }
 };
