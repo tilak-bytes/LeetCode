fef07a7a83abd0df2eb752d3e8b7006a1108fbff
Tilak Verma <engineer.tilak2027@gmail.com>
2026-07-14 13:57:19 +0530

    Time: 14 ms (94.01%), Space: 9.3 MB (89.83%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -44,6 +44,24 @@ private:
         return dp[n][m];
     }
 
+    int solveSO (int n, int m, string& text1, string& text2) {
+        vector<int> prev(m + 1, 0);
+        vector<int> curr(m + 1, 0);
+
+        for(int i = 1 ; i <= n ; i++) {
+            for(int j = 1 ; j <= m ; j++) {
+                if(text1[i - 1] == text2[j - 1]) 
+                    curr[j] = 1 + prev[j - 1];
+
+                else 
+                    curr[j] = max(prev[j], curr[j - 1]);
+            }
+            prev = curr;
+        }
+        return prev[m];
+        
+    }
+
 public:
     int longestCommonSubsequence(string text1, string text2) {
         int n = text1.length();
@@ -53,6 +71,8 @@ public:
         // vector<vector<int>> dp(n + 1, vector<int>(m + 1, -1));
         // return solveMem2(n, m, text1, text2, dp);
 
-        return solveTab(n, m, text1, text2);
+        // return solveTab(n, m, text1, text2);
+
+        return solveSO(n, m, text1, text2);
     }
 };
