0019e2c05f33af7ee86664d155d02aac6f40f5b8
Tilak Verma <engineer.tilak2027@gmail.com>
2026-07-14 13:50:18 +0530

    Time: 26 ms (64.94%), Space: 27.5 MB (53.85%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -18,13 +18,41 @@ private:
         return dp[i][j] = max(solveMem(i - 1, j, text1, text2, dp), solveMem(i, j - 1, text1, text2, dp));
     }
 
+    // Shifting of index by 1.
+    int solveMem2(int i, int j, string& text1, string& text2, vector<vector<int>>& dp) {
+        if(i == 0 || j == 0) return 0;
+
+        if(dp[i][j] != -1) return dp[i][j];
+
+        if(text1[i - 1] == text2[j - 1]) return dp[i][j] = 1 + solveMem2(i - 1, j - 1, text1, text2, dp);
+
+        return dp[i][j] = max(solveMem2(i - 1, j, text1, text2, dp), solveMem2(i, j - 1, text1, text2, dp));
+    }
+
+    int solveTab(int n, int m, string& text1, string& text2) {
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+
+        for(int i = 1 ; i <= n ; i++) {
+            for(int j = 1 ; j <= m ; j++) {
+                if(text1[i - 1] == text2[j - 1]) 
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+
+                else 
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+        return dp[n][m];
+    }
+
 public:
     int longestCommonSubsequence(string text1, string text2) {
         int n = text1.length();
         int m = text2.length();
         // return solveRec(n - 1, m - 1, text1, text2);
 
-        vector<vector<int>> dp(n, vector<int>(m, -1));
-        return solveMem(n - 1, m - 1, text1, text2, dp);
+        // vector<vector<int>> dp(n + 1, vector<int>(m + 1, -1));
+        // return solveMem2(n, m, text1, text2, dp);
+
+        return solveTab(n, m, text1, text2);
     }
 };
