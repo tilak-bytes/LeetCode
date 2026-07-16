acb7c06e8c63ea37a581009b299f8ae4c53dee31
Tilak Verma <engineer.tilak2027@gmail.com>
2026-07-16 12:46:29 +0530

    Time: 0 ms (100%), Space: 20.1 MB (66.42%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -36,11 +36,33 @@ public:
         return dp[0][1];
     }
 
+    int solveSO (vector<int>& prices) {
+        int n = prices.size();
+        vector<int> ahead(2, 0), curr(2, 0);
+
+        int profit;
+        for(int i = n - 1 ; i >= 0 ; i--) {
+            for(int buy = 0; buy <= 1 ; buy++){
+                if(buy) {
+                    profit = max (- prices[i] + ahead[0], 0 + ahead[1]);
+                }
+                else {
+                    profit = max ( prices[i] + ahead[1], 0 + ahead[0]);
+                }
+                curr[buy] = profit;
+            }
+            ahead = curr;
+        }
+        return ahead[1];
+    }
+
     int maxProfit(vector<int>& prices) {
 
         // vector<vector<int> > dp(prices.size(), vector<int> (2, -1));
         // return solveMem(0, 1, prices, dp);
 
-        return solveTab(prices); 
+        // return solveTab(prices); 
+
+        return solveSO(prices);
     }
 };
