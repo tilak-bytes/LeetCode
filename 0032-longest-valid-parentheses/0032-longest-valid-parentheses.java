e8239ca7d3dd088bf07521d7ca3bf2a3440c8194
Tilak Verma <engineer.tilak2027@gmail.com>
2026-07-27 19:31:20 +0530

    Time: 5 ms (81.57%), Space: 46 MB (83.84%) - LeetHub

---
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int longestValidParentheses(String s) {
+        Stack<Integer> st = new Stack<>();
+        int maxLen = 0;
+        st.push(-1);
+        for(int i =0; i< s.length(); i++) {
+            if(s.charAt(i) == '(') {
+                st.push(i);
+            }
+            else {
+                st.pop();
+                if(st.isEmpty()) st.push(i);
+                else maxLen = Math.max(maxLen, i -st.peek());
+            }
+        }
+
+        return maxLen;
+    }
+}
