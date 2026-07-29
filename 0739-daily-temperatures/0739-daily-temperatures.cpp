26b778fe6ad949aa8ae661a83bd2a7ae9c6956db
Tilak Verma <engineer.tilak2027@gmail.com>
2026-07-29 16:37:59 +0530

    Time: 26 ms (36.72%), Space: 102.7 MB (91.36%) - LeetHub

---
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        int n = temperatures.size();
+
+        stack<int> s;
+        vector<int> ans(n, 0);
+
+        for(int i = n - 1 ; i >= 0 ; i--) {
+            while(!s.empty() && temperatures[s.top()] <= temperatures[i]) {
+                s.pop();
+            }
+            ans[i] = !s.empty() ? s.top() - i : 0;
+            s.push(i);
+        }
+        return ans;
+    }
+};
