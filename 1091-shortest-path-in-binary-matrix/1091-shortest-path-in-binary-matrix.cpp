53c6ac0dbae67f4361735647039177c157878d8e
Tilak Verma <engineer.tilak2027@gmail.com>
2026-09-09 13:11:44 +0530

    Time: 12 ms (81.22%), Space: 25.1 MB (59.13%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -2,36 +2,37 @@ class Solution {
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
         int n = grid.size();
-        if(grid[0][0] == 1 || grid[n - 1][n - 1] == 1) return -1;
-        if (n == 1) return 1;
 
-        int dirs[8][2] = {{-1, 0}, {-1, 1}, {0, 1}, {1, 1}, {1, 0}, {1, -1}, {0, -1}, {-1, -1}};
+        if (grid[0][0] == 1 || grid[n - 1][n - 1] == 1)
+            return -1;
+
+        int dirs[8][2] = {{-1, 0}, {-1, 1}, {0, 1},  {1, 1},
+                          {1, 0},  {1, -1}, {0, -1}, {-1, -1}};
 
         vector<vector<bool>> visited(n, vector<bool>(n, false));
+
+        queue<pair<int, pair<int, int>>> q;
+        q.push({0, {0, 0}});
         visited[0][0] = true;
 
-        queue<pair<pair<int, int>, int>> q;
-        q.push({{0, 0}, 1});
-
-        while(!q.empty()) {
-            int i = q.front().first.first;
-            int j = q.front().first.second;
-            int dis = q.front().second;
+        while (!q.empty()) {
+            auto front = q.front();
+            int steps = front.first;
+            auto [row, col] = front.second;
             q.pop();
 
-            for(int k = 0; k < 8; k++) {
-                int ni = i + dirs[k][0];
-                int nj = j + dirs[k][1];
+            if (row == n - 1 && col == n - 1)
+                return steps + 1;
 
-                if(ni >= 0 && ni < n && nj >= 0 && nj < n && 
-                        grid[ni][nj] == 0 && !visited[ni][nj]) {
+            for (auto [i, j] : dirs) {
+                int nr = row + i;
+                int nc = col + j;
 
-                    if (ni == n-1 && nj == n-1) return dis + 1;     // reached!
-
-                    visited[ni][nj] = true;
-                    q.push({{ni, nj}, dis + 1});
+                if (nr >= 0 && nc >= 0 && nr < n && nc < n &&
+                    grid[nr][nc] == 0 && !visited[nr][nc]) {
+                    visited[nr][nc] = true;
+                    q.push({steps + 1, {nr, nc}});
                 }
-
             }
         }
         return -1;
